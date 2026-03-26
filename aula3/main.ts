4bf72440497a77602340522c2a9921c0232c0047
lfalvenga <lfalvenga@gmail.com>
2026-03-26 14:58:03 -0300

    new class - aula 03 - 26/03/2026

---
+++ aula3/main.ts
@@ -0,0 +1,10 @@
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+function saudacao(pessoa : string|null) {
+    return "Ola, " + pessoa;
+}
+
+const nome = prompt("nome: ");
+console.log(saudacao(nome));
